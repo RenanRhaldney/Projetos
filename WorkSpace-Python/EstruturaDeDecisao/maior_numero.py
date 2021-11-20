fc0fc31aba7c5307accd35aafadec8f52dc8977d
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-11-20 10:12:23 -0300

    aumentando o nivel do exercicio

--- WorkSpace-Python/EstruturaDeDecisao/maior_numero.py
+++
@@ -1,7 +0,0 @@
-number1 = float(input("Informed two numbers.\n"))
-number2 = float(input())
-
-if number1 > number2:
-    print(f"Highest number informed is {number1:.2f},")
-else:
-    print(f"Highest number informed is {number2:.2f}")
---
+++ WorkSpace-Python/EstruturaDeDecisao/numero_maior_e_menor.py
@@ -0,0 +1,10 @@
+number1 = float(input("Informed three numbers.\n"))
+number2 = float(input())
+number3 = float(input())
+
+if number1 > number2 and number1 > number3:
+    print(f"Highest number informed is {number1:.2f}")
+elif number2 > number1 and number2 > number3:
+    print(f"Highest number informed is {number2:.2f}")
+else:
+    print(f"Highest number informed is {number3:.2f}")
