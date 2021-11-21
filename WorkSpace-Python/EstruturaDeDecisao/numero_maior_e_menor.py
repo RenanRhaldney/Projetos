83980ecc7bcde89eaab15e2ff1ef765c3379e04c
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-11-21 17:50:32 -0300

    concluindo o exercicio.

--- WorkSpace-Python/EstruturaDeDecisao/numero_maior_e_menor.py
+++ WorkSpace-Python/EstruturaDeDecisao/numero_maior_e_menor.py
@@ -8,3 +8,10 @@ elif number2 > number1 and number2 > number3:
     print(f"Highest number informed is {number2:.2f}")
 else:
     print(f"Highest number informed is {number3:.2f}")
+
+if number1 < number2 and number1 < number3:
+    print(f"Low number informed is {number1:.2f}")
+elif number2 < number1 and number2 < number3:
+    print(f"Low number informed is {number2:.2f}")
+else:
+    print(f"Low number informed is {number3:.2f}")
