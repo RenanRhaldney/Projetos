4b2ef5c60a055a4246c1fa429a0915693b3440a3
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-12-10 18:07:56 -0300

    concluindo exercicio

--- WorkSpace-Python/ExerciciosFaculdade/numeroPrimo.py
+++ WorkSpace-Python/ExerciciosFaculdade/numeroPrimo.py
@@ -1,9 +1,14 @@
 number = int(input("Informe um numero. "))
+contador1 = 0
 
-for i in range(1, number):
+for i in range(1, number + 1):
+    restoDaDivisao = number % i
+    if restoDaDivisao == 0:
+        contador1 +=1
 
-
-
-
+if contador1 > 2:
+    print("NUMERO NAO É PRIMO")
+else:
+    print("NUMERO É PRIMO")
 
 print("FIM...")
