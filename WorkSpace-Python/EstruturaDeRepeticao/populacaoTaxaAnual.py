acb642c3544bb38f7f05ec9db9fcf8176c17fbda
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-12-14 06:11:59 -0300

    adicionado funcao input para entrada de dados e comentario do codigo

--- WorkSpace-Python/EstruturaDeRepeticao/populacaoTaxaAnual.py
+++ WorkSpace-Python/EstruturaDeRepeticao/populacaoTaxaAnual.py
@@ -1,6 +1,7 @@
-populacao1 = 8000
-populacao2 = 20000
+populacao1 = int(input('Populacao do pais A. '))
+populacao2 = int(input('Populacao do pais B. '))
 anos = 0
+#programa informa em quantos anos a populacao1 vai ultrapassar a populacao2
 while populacao1 < populacao2:
     anos += 1
     populacao1 = populacao1 / 100 * 103
