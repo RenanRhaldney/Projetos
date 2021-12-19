2e3de647417b370a3fc114ae1de1c31f816bfc7f
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-12-19 09:00:10 -0300

    invertendo a lista usando list(reserved(nomeDaLista))

---
+++ WorkSpace-Python/ExerciciosListas/listaInvertida.py
@@ -0,0 +1,14 @@
+lista = [11.23, 52.22, 37.54, 11.2, 4.1, 0.23, 8.44, 96.44, 14.68, 73.86]
+contador = -1
+print(lista)
+print(lista[-2])
+
+#print(len(lista))
+#for i in range(-1, -11, -1):
+#    print(lista[i])
+
+#while contador > -11:
+#    print(lista[contador])
+#    contador -= 1
+#O reversed inverte a lista mas tem que usar junto com o list para o list guardar o valor
+print(list(reversed(lista)))
