d2408909f7ae712363a9356a71945c6b225816bd
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-12-19 10:46:56 -0300

    Utilizando a funcao dir na lista

--- WorkSpace-Python/ExerciciosListas/vetorComDados.py
+++ WorkSpace-Python/ExerciciosListas/vetorComDados.py
@@ -1,4 +1,7 @@
 lista = [22, 42, 19, 93, 105, 'feijao']
 
 print(lista)
+#Retorna todos atributos e metodos dentro da lista
+print(dir(lista))
+#informa qual é o tipo da variavel
 print(type(lista))
