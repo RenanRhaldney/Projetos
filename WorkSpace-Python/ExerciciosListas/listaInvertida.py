4770dbefe0ee80c3d294151069b4e2cc1dbd3697
Renan Rhaldney <Renan.rhaldney@gmail.com>
2021-12-20 21:27:26 -0300

    Adicionando mais um tipo de inverter a lista mais simples possivel

--- WorkSpace-Python/ExerciciosListas/listaInvertida.py
+++ WorkSpace-Python/ExerciciosListas/listaInvertida.py
@@ -1,7 +1,6 @@
 lista = [11.23, 52.22, 37.54, 11.2, 4.1, 0.23, 8.44, 96.44, 14.68, 73.86]
 contador = -1
 print(lista)
-print(lista[-2])
 
 #print(len(lista))
 #for i in range(-1, -11, -1):
@@ -12,3 +11,5 @@ print(lista[-2])
 #    contador -= 1
 #O reversed inverte a lista mas tem que usar junto com o list para o list guardar o valor
 print(list(reversed(lista)))
+#start:stop:slep
+print(lista[-1:-11:-1])
