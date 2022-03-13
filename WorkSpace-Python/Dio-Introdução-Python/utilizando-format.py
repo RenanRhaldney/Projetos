3a9cae12ba6ac1d85f744cbac0753247ef6a3a95
Renan Rhaldney <Renan.rhaldney@gmail.com>
2022-03-12 21:16:18 -0300

    outra forma de usar a funcao .format()

--- WorkSpace-Python/Dio-Introdução-Python/utilizando-format.py
+++ WorkSpace-Python/Dio-Introdução-Python/utilizando-format.py
@@ -9,3 +9,9 @@ divisao = a / b
 print('utilizando a função .format().\n'
       'soma = {}\nsubtração = {}\nmultiplicação = {}\ndivisão = {}\nfrase = {}'
       .format(soma, subtracao, multiplicacao, divisao, frase))
+
+print('--------------------------------------//------------------------------------')
+
+print('outra forma de usar a função .format().\n'
+      'soma = {soma}\nsubtração = {sub}\nmultiplicação = {mult}\ndivisão = {div}\nfrase = {frase}'
+      .format(sub=subtracao, frase=frase, div=divisao, mult=multiplicacao, soma=soma))
