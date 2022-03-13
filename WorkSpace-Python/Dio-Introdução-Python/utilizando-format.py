ef5bc7f3aae53e40ff350392948c3d8aeda71521
Renan Rhaldney <Renan.rhaldney@gmail.com>
2022-03-12 21:07:23 -0300

    .format()

---
+++ WorkSpace-Python/Dio-Introdução-Python/utilizando-format.py
@@ -0,0 +1,11 @@
+a = 23
+b = 15
+frase = 'eu sou uma string'
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print('tipos das variaveis iniciadas no escopo global.\n'
+      'soma = {}\nsubtração = {}\nmultiplicação = {}\ndivisão = {}\nfrase = {}'
+      .format(soma, subtracao, multiplicacao, divisao, frase))
