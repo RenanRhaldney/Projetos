344b4310c12ffa8dc11823702be4d307a2b66de2
Renan Rhaldney <Renan.rhaldney@gmail.com>
2022-03-15 16:54:13 -0300

    utilizando listas

---
+++ WorkSpace-Python/Dio-Introdução-Python/aula5.py
@@ -0,0 +1,13 @@
+lista = [1, 3, 88, 16, 33]
+lista_animal = ['cachorro', 'gato', 'passarinho']
+
+print(type(lista_animal))
+print(lista_animal[1])
+
+for i in lista_animal:
+    print(i)
+
+soma = 0
+for i in lista:
+    soma += i
+print(soma)
