d64c6eccdcf67954fa89a7ef51a9028bb81c1473
Renan Rhaldney <Renan.rhaldney@gmail.com>
2022-03-15 16:52:08 -0300

    Codigos bem comentados utilizando funcoes

---
+++ WorkSpace-Python/Dio-Introdução-Python/tupla.py
@@ -0,0 +1,16 @@
+#tupla é uma lista imutavel, não pode ser alterada.
+#para criar uma tubla utilizasse parênteses.
+tupla = (1, 4, 8, 9, 14)
+
+lista_animal = ['cachorro', 'gato', 'passarinho']
+
+#converter uma lista em tupla
+tupla_animal = tuple(lista_animal)
+
+print(type(tupla_animal))
+
+#informa o tanho da tupla
+print(len(tupla))
+
+#retorna o valor na posição informada da lista
+print(tupla[4])
